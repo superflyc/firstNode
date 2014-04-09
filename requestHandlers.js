cbf1d77007320c404c1c2101a6ff0c5c71dae31c
Chris Watkins <starnolds@yahoo.com>
2014-04-08 19:40:32 -0500

    simple HTTP server in node that allows user to upload a .PNG file and display it from nodebeginner.org

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,22 +1,62 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring"),
+	fs = require("fs"),
+	formidable = require("formidable");
 
 function start (response) {
 	console.log("Request handler 'start' was called.");
 
-	exec("ls -lah", function(error, stdout, stderr) {
-		response.writeHead(200,{"Content-Type":"text/plain"});
-		response.write(stdout);
+	//lol it's a servlet
+	//but no worries, only for the purpose of this tutorial
+	var body = '<html>' +
+	'<head>' +
+	'<meta http-equiv="Content-Type" content = "text/html; ' +
+	'<charset=UTF-8" />' +
+	'</head>' +
+	'<body>' +
+	'<form action="/upload" enctype = "multipart/form-data" ' + 
+	'method="post">'+
+	'<input type = "file" name = "upload" multiple = "multiple"/> ' +
+	'<input type="submit" value="Upload File" />'+
+	'</form>'+
+	'</body>'+
+	'</html>';
+	response.writeHead(200,{"Content-Type":"text/html"});
+	response.write(body);
+	response.end();
+
+}
+
+function upload (response,request) {
+	console.log("Request handler 'upload' was called.");
+
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	// how is this non-blocking?
+	form.parse(request, function(error, fields, files) {
+		console.log("parsing done");
+
+		// catch error on Win system when attempting
+		// to rename an existing file
+		fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+			if(error) {
+				fs.unlink("/tmp/test.png");
+				fs.rename(files.upload.path, "/tmp/test.png");
+			}
+		});
+		response.writeHead(200,{"Content-Type":"text/html"});
+		response.write("received image:<br/>");
+		response.write("<img src='/show'/>");
 		response.end();
 	});
 
 }
 
-function upload (response) {
-	console.log("Request handler 'upload' was called.");
-	response.writeHead(200,{"Content-Type":"text/plain"});
-	response.write("Hello Upload");
-	response.end();
+function show (response) {
+	console.log("Request handler 'show' was called.");
+	response.writeHead(200,{"Content-Type":"image/png"});
+	fs.createReadStream("/tmp/test.png").pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ function start(route, handle) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("request for "+pathname+ " received");
 
-		route(handle, pathname, response);
+		route(handle, pathname, response, request);
+
 	}
 
 	http.createServer(onRequest).listen(8888);
